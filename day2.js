e97dff9361be557111adb868d2a60d8e85d6db7d
vincent reddy <vincentreddy25@gmail.com>
2026-02-26 09:34:15 +0530

    javascript from day 1

---
+++ day2.js
@@ -0,0 +1,78 @@
+
+
+// var num1 = 10
+// var num2 = "10"
+
+// if (num1 === num2) {
+//     console.log("hello");
+// } else {
+//     console.log("buchi");
+// }
+
+// ===
+// !==
+
+
+// console.log(2 ** 3);
+
+
+
+
+// var myArray = new Array();
+// myArray[0] = 1
+
+// var myArray = [10, 123.34, false, "hello", null]
+
+// console.log(myArray);
+
+// myArray[1] = 12.34
+
+// myArray.pop()
+// myArray.push("asd")
+
+// myArray.shift()
+// myArray.unshift("first")
+
+
+// console.log(myArray);
+
+
+
+// var obj = {
+//     name: "riya",
+//     age: 22,
+//     myArray: [1, 2, 45.3, "hello"]
+// }
+
+// console.log(obj.myArray);
+
+
+var data = [
+    {
+        name: "product 1",
+        price: 23
+    },
+    {
+        name: "product 2",
+        price: 34
+    }
+]
+
+console.log(data[1].name);
+
+
+
+
+
+// [123, "hello", "bye", false, 733.2, 543.8, true]
+// Find the lenght of the Array without using .lenght
+
+// [0, -34, -1234, 34, 523.5, 74.2]
+// Sort the Array
+
+// [123, "hello", "bye", false, 733.2, 543.8, true]
+// Find the user-Input Element
+
+// [123, "hello", "bye", false, 733.2, 543.8, true]
+// Find the sum of all numbers in the given Array
+

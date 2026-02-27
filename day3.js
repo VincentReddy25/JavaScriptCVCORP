78d8f9140f46c2a71e904fd3f351dd1329b4b61e
vincent reddy <vincentreddy25@gmail.com>
2026-02-27 09:41:39 +0530

    Day 3

---
+++ day3.js
@@ -0,0 +1,35 @@
+
+// function myFun(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+
+// const myFun = function (num1, num2) {
+//     console.log(num1 - num2);
+// }
+
+
+// const myFun = (num1, num2) => {
+//     return num1 - num2
+// }
+// const myFun = (num1, num2) => num1 - num2
+
+
+// myFun(3, 3)
+
+
+
+
+
+
+
+
+let display = document.getElementById("display")
+console.log(display);
+
+let hellosss = document.getElementsByClassName("hello")
+console.log(hellosss);
+
+let paras = document.getElementsByTagName("p")
+console.log(paras);
+

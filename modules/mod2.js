69759161fa32e2a5c704ff6f3eb3ace7516ee67b
vincent reddy <vincentreddy25@gmail.com>
2026-03-03 09:54:32 +0530

    added modules end js

---
+++ modules/mod2.js
@@ -0,0 +1,5 @@
+
+
+export default function sub(num1, num2) {
+    return num1 - num2
+}
---
+++ modules/script.js
@@ -0,0 +1,9 @@
+
+
+import add from "./mod1.js";
+import sub from "./mod2.js";
+
+console.log(add(2, 3));
+console.log(sub(2, 3));
+
+
